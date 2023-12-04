b6b9de247ab933cfeca361114388eb1af1410e1b
iamdeepakram <deepakramsutrakar@gmail.com>
2023-12-04 09:57:11 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -3,22 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+
+    <div className="grid grid-rows-5 grid-cols-6 gap-2"  >
+      <div className="col-start-2 col-end-3 " > <div className={`w-16 h-16 inline-grid rounded-full border-[#E7A09E] border-2 border-purple-600 `} ></div>Base Apparel</div>
+      <div className="col-start-4 col-end-7 row-span-6" >image</div>
+      <div className="col-start-2 row-start-2 row-end-4" >Heading content</div>
+      <div className="col-start-2 row-start-4 row-end-5 open-sans-semi-bold  " >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem psum has been the industry’s standard dummy Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
+      <div className="col-start-2 row-start-5 row-end-5" >email container</div>
+      
     </div>
+  
+    </>
   );
 }
 
