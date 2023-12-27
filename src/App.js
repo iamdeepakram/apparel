b2be921a2d400d1271695857053eac8350aa2f33
iamdeepakram <deepakramsutrakar@gmail.com>
2023-12-27 09:45:55 +0530

    changed ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import girlImg from './img/girlImg.png'
 
 function App() {
   return (
@@ -7,24 +8,29 @@ function App() {
 
     <div className="grid grid-rows-5 grid-cols-6 gap-2"  >
       <div className=" inline-grid col-start-2 col-end-3  " > 
-        <span class="inline-grid grid-cols-2 grid-row-2 ">
-          <span className={`w-16 h-16 rounded-full border-[#E7A09E] border-2 `}  ></span>
-          <span>BASE</span>
-          <span>APPAREL</span>
+        <span className="flex avenir-medium  ">
+          <span className={`w-10 h-10 rounded-full border-[#E7A09E] border-2 place-self-center  `}  ></span>
+          <span className='mt-12 ml-2 flex flex-col avenir-medium  text-[#5F5A5A] ' >
+              <span class="text-lg tracking-widest ">BASE</span>
+              <span class="text-lg tracking-widest	">APPAREL</span>
+          </span>
         </span>
      
       </div>
       <div className="col-start-4 col-end-7 row-span-6" >
-        <img src={logo} ></img>
+        <img src={girlImg} ></img>
       </div>
-      <div className="col-start-2 row-start-2 row-end-4" >
-        <div className="text-7xl tracking-[19.2px] text-were  avenir-light ">WE'RE</div>
-        <p className="text-7xl avenir-medium  text-coming-soon"  >COMING</p>
-        <p className="text-7xl avenir-medium text-coming-soon">SOON</p>
+      <div className="col-start-2 row-start-2 row-end-4 gap-4" >
+        <div className="text-6xl tracking-[19.2px] text-were  avenir-light ">WE'RE</div>
+        <div className="text-6xl tracking-[19.2px] avenir-medium  text-coming-soon"  >COMING</div>
+        <div className="text-6xl tracking-[19.2px] avenir-medium text-coming-soon">SOON</div>
       </div>
       <div className="col-start-2 col-end-4 row-start-4 row-end-5 open-sans-semi-bold text-lorem " >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem psum has been the industry’s standard dummy Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
       <div className="col-start-2 row-start-5 row-end-5" >
-        <input></input>
+        <form>
+        <input className='border rounded-full border-[#9A6B69] w-96 h-14 '  ></input>
+        <button type='submit' className='overflow-visible'  >Submit</button>
+        </form>
       </div>
       
     </div>
